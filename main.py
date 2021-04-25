2e98808bb2e0c6ff458e3383135b919b19ee57ab
RodneyRThomas <82400756+RodneyRThomas@users.noreply.github.com>
2021-04-25 13:06:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 import time
+
+def start():
+  print("I am waiting.")
+  inp = input()
+  if inp == "hello":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "Hello":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "HELLO":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "hi":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "Hi":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "HI":
+    print("Hello my name is Bob")
+    start()
+  elif inp == "open":
+    print("Enter URL and try again")
+    start()
+  elif inp == "Open":
+    print("Enter URL and try again")
+    start()
+  else:
+    print("Sorry, unknown command")
+    start()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+start()
